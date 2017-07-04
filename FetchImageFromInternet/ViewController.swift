b8f6fac866963baa8ca92bafae6359f2663215c8
Zwart <whitetrang1995@gmail.com>
2017-07-04 08:02:42 -0700

    Create new

--- FetchImageFromInternet/ViewController.swift
+++ FetchImageFromInternet/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var urlTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    func fetchImage(_url : String)
+    {
+        let url = URL(string: _url)
+        URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+            
+            //download hit an error so lets return out
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            DispatchQueue.main.async(execute: {
+                
+                if let downloadedImage = UIImage(data: data!) {
+                    self.imageView.image = downloadedImage
+                    self.spinner.stopAnimating()
+                }
+            })
+            
+        }).resume()
 
-
+    }
+    @IBAction func loadImage(_ sender: Any) {
+        fetchImage(_url: urlTextField.text!)
+        spinner.startAnimating()
+    }
 }
 
